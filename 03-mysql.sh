de3c2a2ea67d25c43cc5f110221eea18166ecbe3
manohar <manohar@gmail.com>
2026-05-29 09:27:54 +0000

    Added: Mysql configuration script

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -30,16 +30,12 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>> $LOGS_FILE
-dnf module enable redis:7 -y &>> $LOGS_FILE
-VALIDATE $? "Enabling the Redis 7 Module"
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql-server"
 
-dnf install redis -y &>> $LOGS_FILE
-VALIDATE $? "Installing Redis 7"
+systemctl enable mysqld | tee -a $LOGS_FILE
+systemctl restart mysql
+VALIDATE $? "Restarting mysql-services"
 
-sed -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c/protected-mode no' /etc/redis/redis.conf 
-VALIDATE $? "Enabling remote connections"
-
-systemctl enable redis | tee -a $LOGS_FILE
-systemctl start redis
-VALIDATE $? "Starting the Redis services"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up the root user"
