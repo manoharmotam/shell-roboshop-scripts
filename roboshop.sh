791fb9ce532ac4c3e6034bcdd9c357c5f65657c0
manohar <manohar@gmail.com>
2026-05-29 07:28:55 +0000

    Updated: Instance creation script

--- roboshop.sh
+++ roboshop.sh
@@ -63,10 +63,10 @@ do
                         {
                         "Action": "UPSERT",
                         "ResourceRecordSet": {
-                            "Name": "$R53_RECORD",
+                            "Name": "'$R53_RECORD'",
                             "Type": "A",
                             "TTL": 1,
-                            "ResourceRecords": [{ "Value": "$IP" }]
+                            "ResourceRecords": [{ "Value": "'$IP'" }]
                         }
                     }
                 ]
